fab34b4e7d3e55650ba04b18916e5fd617b4892b
sdkingsley <skingsl2@u.rochester.edu>
2016-10-11 12:20:27 -0400

    Revert "something went wrong"
    This reverts commit 808f2652a1a13ddbac4b79a2489ca2cb785ccb74.

---
+++ finearts.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<?php 
+  $thisPage = "FineArts"
+?>
+
+<?php 
+  include "inc/head.inc"
+?>
+  
+  <body>
+    <div class="wrapper">
+      <header>
+        <h1>Sarah D. Kingsley</h1>
+        <p>Senior at the University of Rochester studying Human Computer Interaction and Studio Arts.</p>
+        <?php 
+          include "inc/nav.inc"
+        ?>
+      </header>
+      <section>
+        <h3>
+<a id="welcome-to-github-pages" class="anchor" href="#welcome-to-github-pages" aria-hidden="true"><span aria-hidden="true" class="octicon octicon-link"></span></a>Software Engineering</h3>
+
+<p>At the University of Rochester I am fulfilling a bachelor of arts degree in human computer interaction. Through this course of study I have learned many skills, my main proficiencies being in Java, web development, mobile development and user experience research.</p>
+
+<p class="view"><a href="https://github.com/sdkingsley">View My GitHub Profile</a></p>
+<h3>
+<a id="designer-templates" class="anchor" href="#designer-templates" aria-hidden="true"><span aria-hidden="true" class="octicon octicon-link"></span></a>User Interface Design</h3>
+
+<p>With a background in the fine arts, human computer interaction and an accompanying business minor, I believe I have a unique ability to be able to combine those skills in the field of user interface design. Creating visually has always been my passion, and I observe interfaces as a medium where I can work actively with with the process of thought.</p>
+<h3>
+<a id="creating-pages-manually" class="anchor" href="#creating-pages-manually" aria-hidden="true"><span aria-hidden="true" class="octicon octicon-link"></span></a>Graphic Design and Marketing</h3>
+
+<p>I am proficient in the Adobe Suite, with an equal preference between Illustrator and Photoshop. With these tools I have had opportunities to create print and social media campaigns through a full time internship under the head of marketing at IP.com. I have also had the opportunity to be a leader in communications for three different student organizations.</p>
+<h3>
+<a id="authors-and-contributors" class="anchor" href="#authors-and-contributors" aria-hidden="true"><span aria-hidden="true" class="octicon octicon-link"></span></a>Fine Arts</h3>
+
+<p>In my art I use mediums of acrylic paint and digital to take a closer look at the role of empathy in reference to the voices within myself and within the networks I operate. In the past this has included looking at the art form of theatre, the complexities of proportion, tensions between technologies and humanity, and advocation for sexual assault awareness. My current work involves painting portraits of women in all the STEM majors that the University of Rochester to purpose both celebrating their passion and understanding the difficulties of being in that position.</p>
+    
+  </body>
+</html>
